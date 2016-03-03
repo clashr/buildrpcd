bc3f2bb67f5503b772ce006b02a2cefb5311a65e
Dennis Chen <barracks510@gmail.com>
2016-03-02 21:54:23 -0500

    api: quick refactor.

--- api/api.go
+++ api/api.go
@@ -1,11 +1,5 @@
 package api
 
-import (
-	"log"
-
-	"github.com/clashr/buildrpcd/services"
-)
-
 type Args struct {
 	Language, Dialect, Contents string
 }
@@ -17,16 +11,6 @@ type Result struct {
 	CCOut, Binary []byte
 }
 
-func (b *Build) Compile(args Args, result *Result) (err error) {
-	return Compile(args, result)
-}
-
-func Compile(args Args, result *Result) (err error) {
-	log.Printf("Reached Compile Endpoint\n")
-	binary, output, err := services.Make(args.Contents, args.Language, args.Dialect)
-	if err != nil {
-		return
-	}
-	*result = Result{output, binary}
-	return nil
+func (b *Build) Compile(args Args, result *Result) error {
+	return compile(args, result)
 }
---
+++ api/compile.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+	"log"
+
+	"github.com/clashr/buildrpcd/services"
+)
+
+func compile(args Args, result *Result) error {
+	log.Printf("Reached Compile Endpoint\n")
+	binary, output, err := services.Make(args.Contents, args.Language, args.Dialect)
+	if err != nil {
+		return err
+	}
+	*result = Result{output, binary}
+	return nil
+}
